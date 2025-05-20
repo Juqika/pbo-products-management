c706645fb1583fc800d14a33febb22520cc9a307
indromhars <indra.madaharsa@gmail.com>
2025-05-20 09:07:24 +0700

    adding interface

---
+++ products/src/dao_interface/VendorInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao_interface;
+
+import java.util.List;
+import model.Vendor;
+
+/**
+ *
+ * @author indrovert
+ */
+public interface VendorInterface {
+    
+}
