283f4a7678e3dc1898f753e10dd31db99b30f255
kingilham <ilhammaulan405@gmail.com>
2025-05-23 23:43:16 -0700

    Update StoreProductDAO.java

--- products/src/dao/StoreProductDAO.java
+++ products/src/dao/StoreProductDAO.java
@@ -15,6 +15,76 @@ import java.util.List;
  *
  * @author windows 10
  */
-public class StoreProductDAO {
+public class StoreProductDAO implements StoreProductInterface {
+    Connection conn;
     
+    public StoreProductDAO() {
+        conn = connect.getcC();
+    }
+    
+    @Override
+    public  List<StoreProduct> getProduct (int idStore) {
+        List<StoreProduct> productList = new ArrayList<>();
+        String sql = "SELECT * FROM store_product WHERE id_store = ?";
+        
+        try (PreparedStatement st = conn.prepareStatement(sql)) {
+            st.setInt(1, idStore);
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()) {
+                StoreProduct sp = new StoreProduct();
+                sp.setIdStoreProduct(rs.getInt("id_store_product"));
+                sp.setIdStore(rs.getInt("id_store"));
+                sp.setIdProduct(rs.getInt("id_product"));
+                productList.add(sp);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error getting products by store ID: \" + e.getMessage()");
+        }
+        return productList;
+    }
+    @Override
+    public boolean addProduct (int idStore, int idProduct) {
+        String sql = "INSERT INTO store_product (id_store, id_product) VALUES (?, ?)";
+        try (PreparedStatement st = conn.prepareStatement(sql)){
+            st.setInt(1, idStore);
+            st.setInt(2, idProduct);
+            int rowsAffected = st.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.println("Error adding product to store: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    
+@Override
+public boolean deleteProduct(int idStore, int idProduct) {
+    String sql = "DELETE FROM store_products WHERE id_store = ? AND id_product = ?";
+    try (PreparedStatement st = conn.prepareStatement(sql)) {
+        st.setInt(1, idStore);
+        st.setInt(2, idProduct);
+        int rowsAffected = st.executeUpdate();
+            return rowsAffected > 0;
+    } catch (SQLException e) {
+        System.out.println("Error deleting product from store: " + e.getMessage());
+        return false;
+    }
+}
+    
+ @Override
+public boolean getById(int idStore, int idProduct) {
+    String sql = "SELECT 1 FROM store_product WHERE id_store = ? AND id_product = ?";
+
+    try (PreparedStatement st = conn.prepareStatement(sql)) {
+        st.setInt(1, idStore);
+        st.setInt(2, idProduct);
+        ResultSet rs = st.executeQuery();
+        return rs.next(); // true jika ada data
+    } catch (Exception e) {
+        System.out.println("Error checking product in store: " + e.getMessage());
+        return false;
+    }
+  }
 }
