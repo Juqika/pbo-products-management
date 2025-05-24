b110a58c4c114040e7feb03671e108fb30ade50e
kingilham <ilhammaulan405@gmail.com>
2025-05-24 04:52:26 -0700

    Update BookingInterface.java

--- products/src/dao_interface/BookingInterface.java
+++ products/src/dao_interface/BookingInterface.java
@@ -3,16 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package dao_interface;
-import java.util.List;
-import model.Booking;
+
 /**
  *
  * @author indrovert
  */
 public interface BookingInterface {
-    void insert (Booking b);
-    void update (Booking b);
-    void delete (int id);
-    List<Booking> getAll();
-    Booking getByid (int id);
+    
 }
