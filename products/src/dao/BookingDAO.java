b848f3cfb5b0efb59ae36cd0c6122cb3ca19107c
kingilham <ilhammaulan405@gmail.com>
2025-05-24 04:36:59 -0700

    Update BookingDAO.java

--- products/src/dao/BookingDAO.java
+++ products/src/dao/BookingDAO.java
@@ -3,16 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
-import java.util.List;
-import model.Booking;
+
 /**
  *
  * @author windows 10
  */
 public class BookingDAO {
-    void insert (Booking b);
-    void update (Booking b);
-    void delete (int id);
-    List<Booking> getAll();
-    Booking getByid (int id);
+    
 }
