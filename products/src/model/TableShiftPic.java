49b9bced4f88cd07c6503ca41eaaf895e93c3329
Juqika <rizkybanu1@gmail.com>
2025-06-05 00:09:56 +0700

    add TableShiftPic.java di model

---
+++ products/src/model/TableShiftPic.java
@@ -0,0 +1,56 @@
+package model;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class TableShiftPic extends AbstractTableModel {
+    List<shift_pic> sp;
+    
+    public TableShiftPic(List<shift_pic> sp) {
+        this.sp = sp;
+    }
+    
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return sp.size();
+    }
+    
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:
+                return "id_shift";
+            case 1:
+                return "name";
+            case 2:
+                return "start_check_time";
+            case 3:
+                return "end_check_time";
+            case 4:
+                return "note";
+            default:
+                return null;
+        }
+    }
+    
+    public Object getValueAt(int row, int column) {
+        switch (column) {
+            case 0:
+                return sp.get(row).getIdShift();
+            case 1:
+                return sp.get(row).getName();
+            case 2:
+                return sp.get(row).getStart_check_time();
+            case 3:
+                return sp.get(row).getEnd_check_time();
+            case 4:
+                return sp.get(row).getNote();
+            default:
+                return null;
+        }
+    }
+
+}
