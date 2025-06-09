74b01057659bdfcaa36dcf3f95781e3de4a2f88e
Juqika <rizkybanu1@gmail.com>
2025-06-10 01:58:57 +0700

    delete yang tidak perlu

--- products/src/view/ShiftPicView.java
+++ products/src/view/ShiftPicView.java
@@ -246,17 +246,6 @@ public class ShiftPicView extends javax.swing.JFrame {
     
         public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ShiftPicView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
